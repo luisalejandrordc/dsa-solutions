cf14d4ce1ceb01f42398ffc43a85c818373d55ef
Luis Alejandro <luisalejandrordc@gmail.com>
2026-08-25 01:02:27 -0500

    add IT-Hub/Trees/1_2.cpp solution

---
+++ IT-Hub/Trees/1/1_2.cpp
@@ -0,0 +1,33 @@
+#include "../../../include/binary_tree.h"
+#include "../../../include/utils.h"
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+TreeNode *invertTree(TreeNode *root) {
+  queue<TreeNode *> children;
+  if (root != nullptr)
+    children.push(root);
+  while (!children.empty()) {
+    TreeNode *curr = children.front();
+    swap(curr->left, curr->right);
+    if (curr->left != nullptr)
+      children.push(curr->left);
+    if (curr->right != nullptr)
+      children.push(curr->right);
+    children.pop();
+  }
+  return root;
+}
+
+int main() {
+  printTitle("Invert Binary Tree");
+  vector<int> nums = {4, 2, 7, 1, 3, 6, 9};
+  TreeNode *root = arrayToBinaryTree(nums);
+  TreeNode *solution = timedCall(invertTree, root);
+  cout << "Solution: ";
+  printBinaryTree(solution); // actually root = solution
+  return 0;
+}
